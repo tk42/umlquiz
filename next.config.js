151329be4b3b05aae1621221d352a9cbfb7612b4
tk42 <nsplat@gmail.com>
2022-05-03 15:13:53 +0000

    Reverted next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')();
 module.exports = removeImports({
-    // reactStrictMode: true,
+    reactStrictMode: true,
     // pageExtensions: ['page.tsx', 'page.ts'],
     images: {
       domains: [],
     },
     swcMinify: true,
-    i18n: {
-      locales: ["en", "ja"],
-      defaultLocale: "ja",
-    },
+    // i18n: {
+    //   locales: ["en", "ja"],
+    //   defaultLocale: "en",
+    // },
 });
