bd56a5b07c1b2b2227897e59e76a1382efe37d35
tk42 <tk42@users.noreply.github.com>
2022-05-03 14:35:01 +0000

    deploy: d654f6cad3e17b1e7995fc1abe050d9c0aa13d41

---
+++ _next/static/gEcl-FmpcL37BDqoU8n_U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/eb1842f2-499572dadb6d33e0.js","static/css/f02faecfac228b32.css","static/chunks/pages/index-553b53444a1c3773.js"],"/_error":["static/chunks/pages/_error-ce9f51dab56f7936.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
