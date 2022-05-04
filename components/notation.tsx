2c5135ea3bf7374a634ef115d5cd11f098791a0f
tk42 <nsplat@gmail.com>
2022-05-04 05:01:11 +0000

    Refactor Notation component

---
+++ components/notation.tsx
@@ -0,0 +1,82 @@
+import { useTranslation } from "next-export-i18n";
+import { Typography, Grid } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import UMLPreviewer from "./umlpreview";
+
+
+const notation_visibility = `class Visibility
+Visibility : +int public_member
+Visibility : #int protected_member
+Visibility : -int private_member
+Visibility : ~int package_member`;
+
+const notation_relationships = `classA0 --|> classB0 : Inheritance
+classA1 --* classB1 : Composition
+classA2 --o classB2 : Aggregation
+classA3 --> classB3 : Association
+classA4 ..> classB4 : Dependency
+classA5 ..|> classB5 : Realization`;
+
+const notation_sequences = `Alice->>+John: Hello John, how are you?
+Alice-->>+John: John, can you hear me?
+John-)-Alice: Hi Alice, I can hear you! (async)
+John--)-Alice: I feel great! (async)`;
+
+
+export default function Notation() {
+    const { t } = useTranslation();
+    return (
+      <div className="glass">
+        <Typography variant={"h2"}>{t("notation")}</Typography>
+        <Typography variant={"h3"}>{t("caption_classdiagram")}</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Visibility"
+              multiline
+              rows={5}
+              fullWidth
+              defaultValue={notation_visibility}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <UMLPreviewer value={notation_visibility} prefix={"classDiagram"} />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Relationships"
+              multiline
+              rows={6}
+              fullWidth
+              defaultValue={notation_relationships}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <UMLPreviewer
+              value={notation_relationships}
+              prefix={"classDiagram"}
+            />
+          </Grid>
+        </Grid>
+        <Typography variant={"h3"}>{t("caption_sequencediagram")}</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              label="Sequence"
+              multiline
+              rows={4}
+              fullWidth
+              defaultValue={notation_sequences}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <UMLPreviewer
+              value={notation_sequences}
+              prefix={"sequenceDiagram"}
+            />
+          </Grid>
+        </Grid>
+      </div>
+    );
+  };
+  
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,12 @@
-import {
-  useTranslation,
-} from "next-export-i18n";
+import { useTranslation } from "next-export-i18n";
 
-import Link from 'next/link';
+import Link from "next/link";
 
 import UMLPreviewer from "../components/umlpreview";
+import Notation from "../components/notation";
 import { Typography, Button, Grid } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
-
-const notation_visibility = `class Visibility
-Visibility : +int public_member
-Visibility : #int protected_member
-Visibility : -int private_member
-Visibility : ~int package_member`;
-
-const notation_relationships = `classA0 --|> classB0 : Inheritance
-classA1 --* classB1 : Composition
-classA2 --o classB2 : Aggregation
-classA3 --> classB3 : Association
-classA4 ..> classB4 : Dependency
-classA5 ..|> classB5 : Realization`;
-
-const notation_sequences = `Alice->>+John: Hello John, how are you?
-Alice-->>+John: John, can you hear me?
-John-)-Alice: Hi Alice, I can hear you! (async)
-John--)-Alice: I feel great! (async)`;
 
 const mdMermaid = `Animal <|-- Duck
 Animal <|-- Fish
@@ -46,16 +27,10 @@ class Fish{
 class Zebra{
     +bool is_wild
     +run()
-}
-`;
+}`;
 
 function HomePage() {
   const { t } = useTranslation();
-  const [lang, setLang] = useState('en');
-
-  // const handleChangeLang = (event: SelectChangeEvent) => {
-  //   setLang(event.target.value as string);
-  // };
 
   const [value, setValue] = useState(mdMermaid);
 
@@ -66,82 +41,39 @@ function HomePage() {
   };
 
   return (
-      <div className="bg">
-        <Typography variant={"h1"}>UML Quiz</Typography>
-        <p>
-          <Link href={"/?lang=en"}>English</Link>/<Link href={"/?lang=fr"}>Français</Link>/<Link href={"/?lang=ja"}>日本語</Link>
-        </p>
-        <p>{t('intro')}</p>
-        <div className="glass">
-          <Typography variant={"h2"}>{t('notation')}</Typography>
-          <Typography variant={"h4"}>{t('caption_classdiagram')}</Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Visibility"
-                multiline
-                rows={5}
-                fullWidth
-                defaultValue={notation_visibility}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <UMLPreviewer value={notation_visibility} prefix={"classDiagram"} />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Relationships"
-                multiline
-                rows={6}
-                fullWidth
-                defaultValue={notation_relationships}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <UMLPreviewer
-                value={notation_relationships}
-                prefix={"classDiagram"}
-              />
-            </Grid>
-          </Grid>
-          <Typography variant={"h4"}>{t('caption_sequencediagram')}</Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                label="Sequence"
-                multiline
-                rows={4}
-                fullWidth
-                defaultValue={notation_sequences}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <UMLPreviewer value={notation_sequences} prefix={"sequenceDiagram"} />
-            </Grid>
-          </Grid>
-        </div>
-        <div className="glass">
-          <Typography variant={"h2"}>Q1</Typography>
-          <p>
-            ある会員制のホテルは15部屋保有しています．予約は一人の会員に対して，1回に1部屋だけ受け付けます．適切なクラス図を描きましょう．
-          </p>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                value={value}
-                // placeholder={"Input classdiagram"}
-                multiline
-                fullWidth
-                onChange={handleChangeMarkdown}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <UMLPreviewer value={value} prefix={"classDiagram"} />
-            </Grid>
-          </Grid>
-          <Button>Submit</Button>
-        </div>
+    <div className="bg">
+      <Typography variant={"h1"}>UML Quiz</Typography>
+      <p>
+        <Link href={"/?lang=en"}>English</Link>/
+        <Link href={"/?lang=fr"}>Français</Link>/
+        <Link href={"/?lang=ja"}>日本語</Link>
+      </p>
+      <div className="glass">
+        {t("intro")}
       </div>
+      <Notation />
+      <div className="glass">
+        <Typography variant={"h2"}>Q1</Typography>
+        <p>
+          ある会員制のホテルは15部屋保有しています．予約は一人の会員に対して，1回に1部屋だけ受け付けます．適切なクラス図を描きましょう．
+        </p>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              value={value}
+              // placeholder={"Input classdiagram"}
+              multiline
+              fullWidth
+              onChange={handleChangeMarkdown}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <UMLPreviewer value={value} prefix={"classDiagram"} />
+          </Grid>
+        </Grid>
+        <Button>Submit</Button>
+      </div>
+    </div>
   );
 }
 
