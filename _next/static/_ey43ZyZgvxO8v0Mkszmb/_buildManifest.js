30dffe8a8326443e71c6f4105061494c19921094
tk42 <tk42@users.noreply.github.com>
2022-05-04 13:32:35 +0000

    deploy: 5b8d30b33bb636bb1248d6b7f48b0af06055dad9

--- _next/static/_ey43ZyZgvxO8v0Mkszmb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/eb1842f2-499572dadb6d33e0.js","static/css/b5e6cd5b6a6247fd.css","static/chunks/pages/index-372ad6319155b945.js"],"/_error":["static/chunks/pages/_error-ce9f51dab56f7936.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
